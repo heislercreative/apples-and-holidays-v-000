efa55e8b27f3cbd04983cdfc95397024b8f2117f
Brennan Heisler <learn@heislercreative.com>
2018-01-30 02:27:29 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 
   holiday_hash.each do |season, holiday_hashes|
-    season.to_s.split each do |words|
+    season.to_s.split.each do |words|
       words.capitalize!.join
     end
   end
