668e1aadd988913107ef2ebed5b794751bea2e99
Brennan Heisler <learn@heislercreative.com>
2018-01-30 02:54:54 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,7 +83,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.collect do |season, holiday|
     season.collect do |holiday, supplies|
       if supplies.include?("BBQ")
-        holiday.keys
+        holiday
       end
     end
   end
